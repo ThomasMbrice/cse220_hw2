e7fdab740a96deb6591c8cb9e2d752c9d68d6946
Thomas <145804162+ThomasMbrice@users.noreply.github.com>
2024-02-25 18:15:26 +0000

    submite 1

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -15,8 +15,8 @@ int main(int argc, char **argv) {
     char *output_file = NULL;
     char *copy_arg = NULL;
     char *paste_arg = NULL;
-    char *message_arg = NULL;
-    //FILE *ip, *op;
+    //char *message_arg = NULL;
+    FILE *ip, *op;
     int repeatedoutflag = 0, repeatedinflag = 0, repeatcopyflag = 0, 
     repeatpastflag = 0, repeatmessageflag =0; 
 
@@ -29,41 +29,79 @@ int main(int argc, char **argv) {
 
         } else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) {
             output_file = argv[++i];
-            printf("how mant runs.  %s \n", output_file);
             repeatedoutflag++;
 
         } else if (strcmp(argv[i], "-c") == 0 && i + 1 < argc) {
             copy_arg = argv[++i];
             repeatcopyflag++;
+            int temp = 0;
+            for(unsigned long e = 0; e < strlen(copy_arg); e++){        //checks copyarg is valid
+                if(strcmp(&copy_arg[e], ",") == 0){
+                    temp++;
+                    if(!isdigit(&paste_arg[e+1])){
+                        temp = -1;
+                        break;
+                    }
+                }
+            }
+            if(temp != 3)                                        // causes error to throw later
+                copy_arg = NULL;
 
         } else if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
             paste_arg = argv[++i];
             repeatpastflag++;
+            int temp = 0;
+            for(unsigned long e = 0; e < strlen(copy_arg); e++){        //checks copyarg is valid
+                if(strcmp(&paste_arg[e], ",") == 0){
+                    temp++;
+                    if(!isdigit(&paste_arg[e+1])){
+                        temp = -1;
+                        break;
+                    }
+                }
+            }
+            if(temp != 1)                                        // causes error to throw later
+                copy_arg = NULL;
 
         } else if (strcmp(argv[i], "-r") == 0 && i + 1 < argc) {
-            message_arg = argv[++i];
+            //message_arg = argv[++i];
             repeatmessageflag++;
+
         }
+        else
+            return UNRECOGNIZED_ARGUMENT;               //UNRECOGNIZED_ARGUMENT
     }
 
     if((input_file == NULL) | (output_file == NULL))    //MISSING_ARGUMENT
         return MISSING_ARGUMENT;
-    //UNRECOGNIZED_ARGUMENT
-    if((input_file != NULL && repeatedinflag != 1) | (output_file != NULL && repeatedoutflag != 1) |
-    (copy_arg != NULL && repeatcopyflag != 1) | (paste_arg != NULL && repeatpastflag != 1) |
-    (message_arg != NULL && repeatmessageflag != 1))    //DUPLICATE_ARGUMENT
+
+    else if((repeatedinflag > 1) | (repeatedoutflag > 1) | (repeatcopyflag > 1) 
+    | (repeatpastflag > 1) |(repeatmessageflag > 1))    //DUPLICATE_ARGUMENT
         return DUPLICATE_ARGUMENT;
-    //if ((ip = fopen(input_file, "r")) == NULL)          //INPUT_FILE_MISSING
-    //    return INPUT_FILE_MISSING;
-    //if ((op = fopen(output_file, "w")) == NULL)         //OUTPUT_FILE_UNWRITABLE
-    //    return OUTPUT_FILE_UNWRITABLE;
-    if((repeatpastflag == 1) && (repeatcopyflag == 0))
+
+    else if ((ip = fopen(input_file, "r")) == NULL)          //INPUT_FILE_MISSING
+        return INPUT_FILE_MISSING;
+
+    else if ((op = fopen(output_file, "w")) == NULL)         //OUTPUT_FILE_UNWRITABLE
+        return OUTPUT_FILE_UNWRITABLE;
+
+    else if((repeatpastflag == 1) && (repeatcopyflag == 0))
         return C_ARGUMENT_MISSING; 
-    // C_ARGUMENT_INVALID
-    //P_ARGUMENT_INVALID
-    //R_ARGUMENT_INVALID
+
+    else if((repeatcopyflag > 0) && copy_arg == NULL)               //C_ARGUMENT_INVALID
+        return C_ARGUMENT_INVALID;
+    else if((repeatpastflag > 0) && paste_arg == NULL)                    //P_ARGUMENT_INVALID
+        return P_ARGUMENT_INVALID;
+    //else if()                       //R_ARGUMENT_INVALID
+    //    return R_ARGUMENT_INVALID;
 
     return 0;
 }
 
-    
+/*
+    else if((input_file != NULL && repeatedinflag != 1) | (output_file != NULL && repeatedoutflag != 1) |
+    (copy_arg != NULL && repeatcopyflag != 1) | (paste_arg != NULL && repeatpastflag != 1) |
+    (message_arg != NULL && repeatmessageflag != 1))    //DUPLICATE_ARGUMENT
+        return DUPLICATE_ARGUMENT;
+*/
+
